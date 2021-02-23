65abede1c3692b0043ddad0250c061141376a2b4
Sheriff-A <manshari619@yahoo.com>
2021-02-23 15:53:52 -0500

    Implemented userID, email and renamed

--- Login_Page_Demo/demoRegisterHandler.js
+++ Login_Page_Demo/demoRegisterHandler.js
@@ -7,15 +7,16 @@ app.get('/demoLoginPage.htm', function (req, res) { // specific get url
    res.sendFile( __dirname + "/public/" + "form.html" ); // returns this page to the on the get addr
 })
 
-app.get('/process_login', function (req, res) {
-   // Prepare output in JSON format
+app.get('/process_register', function (req, res) {
+   // Prepare output in JSON format (Dictionary)
    var response = {
       username: req.query.username, // gotten from the form
+      email: req.query.email, // gotten from form
       password: scramble(req.query.password), // gotten from the form
       allergens: req.query.allergens, // gotten from the form
       salt: generateSalt() // To be used in hashing the password
    };
-   // Print the query
+   // Print
    console.log(response); // Display the form information
 
    // // Start of SQL
@@ -36,19 +37,22 @@ app.get('/process_login', function (req, res) {
 
         // create Request object
         var request = new sql.Request();
-           
-        var queryString = `INSERT INTO dbo.[user] 
-        VALUES (99, '${response.username}', '${response.password}', 110, '${response.salt}')`
-        var queryString2 = 'SELECT * FROM [user]'
-        request.query(queryString, function (err, recordset) {
-            
-            if (err) console.log(err)
 
-            // Print Response that the user was successfully added...
-            // This is flawed as it's a success print if the query went through
-            // NOT if the query succeeded
-            console.log('Successfully added %s to the database', response.username)
+        // Query to get max user ID to increment uid for new user
+        var queryString1 = 'SELECT MAX(user_id) FROM dbo.[user]'
+        request.query(queryString1, function (err, result) {
+            if (err) console.log(err);
+            response.uid = result.recordset[0][''] + 1;
             
+            // Second Query once new uid is prepared
+            // This query adds the user to the database with the new userID
+            var queryString2 = `INSERT INTO dbo.[user] 
+            VALUES ('${response.uid}', '${response.username}', '${response.password}', 110, '${response.salt}', '${response.email}')`
+            request.query(queryString2, (err2, result2) => {
+               if (err2) console.log(err2)
+               console.log('UID: ' + response.uid);
+               console.log('Successfully added %s to the database', response.username);
+            })
         });
     });
     // End of SQL
@@ -57,7 +61,7 @@ app.get('/process_login', function (req, res) {
     * This is the post operation after all the handling
     * ideally, it would leading to the home page or account page or whatever
     */
-   res.end(JSON.stringify(response)); // Display the information to the webpage
+   res.end(JSON.stringify(response));
 })
 
 var server = app.listen(8081, function () {
@@ -65,14 +69,16 @@ var server = app.listen(8081, function () {
    var port = server.address().port
    
    console.log("Example app listening at http://localhost:%s", port)
-   console.log('Go to http://localhost:%s/demoLoginPage.html to test', port)
+   console.log('Go to http://localhost:%s/demoRegisterPage.html to test', port)
 })
 
 function scramble(password, salt){
-    // A properly complicated scramble (hashing) function needs to implemented
+    // A properly complicated scramble (hashing) function
+    // Double hashed with salt
     return md5(md5(password) + salt)
 }
 
 function generateSalt(){
+   // Goal: Make a randomly generated salt string
    return 'iAmSalt'
 }

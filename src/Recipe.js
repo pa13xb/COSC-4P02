fadfdd00e20023cdac41852148e2087873fadc7f
karina-b <ksb_05@hotmail.com>
2021-02-22 22:29:31 -0500

    Add files via upload

---
+++ src/Recipe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recipe = () => {
+    return(
+        <div>
+            <h1>Title</h1>
+            <img src="" alt=""/>
+            <p> Calories</p>
+        </div>
+    );
+}
+export default Recipe;
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Recipes',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login/Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
